d40eb67dcad11535500f96129afb2956f35342bc
FlimFlamer <dronik61@gmail.com>
2022-12-10 01:48:43 +0300

    1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,30 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Задание 1");
+        System.out.println();
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(i);
+        }
+
+        System.out.println();
+        System.out.println("Задание 2");
+        System.out.println();
+        for (int b = 10; b > 0; b = b - 1) {
+            System.out.println(b);
+        }
+
+        System.out.println();
+        System.out.println("Задание 3");
+        System.out.println();
+        for (int c = 0; c < 17; c = c + 2) {
+            System.out.println(c);
+        }
+
+        System.out.println();
+        System.out.println("Задание 4");
+        System.out.println();
+        for (int d = 10; d > -11; d= d - 1) {
+            System.out.println(d);
+        }
     }
 }
