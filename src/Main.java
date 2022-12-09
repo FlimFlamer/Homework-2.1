c5bb08f5833faffc53427b23c91a94bc87952dc0
FlimFlamer <dronik61@gmail.com>
2022-12-10 02:26:24 +0300

    1

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,10 @@
+import java.util.ArrayList;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Задание 1");
         System.out.println();
-        for (int i = 1; i <= 10; i++) {
+        for (int i = 1; i < 10; i++) {
             System.out.println(i);
         }
 
@@ -23,8 +25,36 @@ public class Main {
         System.out.println();
         System.out.println("Задание 4");
         System.out.println();
-        for (int d = 10; d > -11; d= d - 1) {
+        for (int d = 10; d > -10; d= d - 1) {
             System.out.println(d);
         }
+
+        System.out.println();
+        System.out.println("Задание 5");
+        System.out.println();
+        for (int leapYear = 1904; leapYear < 2097; leapYear = leapYear +4) {
+            System.out.println(leapYear + " год является високосным.");
+        }
+
+        System.out.println();
+        System.out.println("Задание 6");
+        System.out.println();
+        for (int number = 7; number < 99; number = number + 7) {
+            System.out.println(number);
+        }
+
+        System.out.println();
+        System.out.println("Задание 8");
+        System.out.println();
+        for (int number1 = 1; number1 < 513; number1 = number1 *2) {
+            System.out.println(number1);
+        }
+
+        System.out.println();
+        System.out.println("Задание 8");
+        System.out.println();
+        int contribution = 29000;
+        int sumCont = contribution;
+
     }
 }
